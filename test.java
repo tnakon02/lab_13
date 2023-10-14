1fe24b110a6f4bd8971de8aa204e021f59ff5eca
Thanaphon Rawipasakorn <thanaphon.r@kkumail.com>
2023-10-14 22:03:54 +0700

    test2

--- test.java
+++ test.java
@@ -1,11 +1,17 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-public class SumOfDigitsTest {
+public class test {
 
     @Test
     public void testSumOfDigits() {
         assertEquals(9, SumOfDigits.calculateSumOfDigits(12345)); // 1+2+3+4+5 = 15 -> 1+5 = 6
         assertEquals(3, SumOfDigits.calculateSumOfDigits(987654321)); // 9+8+7+6+5+4+3+2+1 = 45 -> 4+5 = 9
     }
+    @Test
+    public void testSingleDigitInput() {
+        // Test with a single-digit input
+        assertEquals(5, SumOfDigits.calculateSumOfDigits(5));
+    }
+
 }
