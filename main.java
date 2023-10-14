7168b693dd9b0fb78972f3ed694f07fab878a020
supattra pangjun <supattra.pa@kkumail.com>
2023-10-14 21:48:00 +0700

    first commit

---
+++ main.java
@@ -0,0 +1,13 @@
+public class SumOfDigits {
+    public static int calculateSumOfDigits(int n) {
+        while (n >= 10) {
+            int sum = 0;
+            while (n > 0) {
+                sum += n % 10;
+                n /= 10;
+            }
+            n = sum;
+        }
+        return n;
+    }
+}
