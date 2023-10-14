ee007f0b90f002fb2f75bf680e1211d16bed3016
supattra pangjun <supattra.pa@kkumail.com>
2023-10-14 22:06:27 +0700

    test3

--- test.java
+++ test.java
@@ -5,13 +5,15 @@ public class test {
 
     @Test
     public void testSumOfDigits() {
-        assertEquals(9, SumOfDigits.calculateSumOfDigits(12345)); // 1+2+3+4+5 = 15 -> 1+5 = 6
-        assertEquals(3, SumOfDigits.calculateSumOfDigits(987654321)); // 9+8+7+6+5+4+3+2+1 = 45 -> 4+5 = 9
+        assertEquals(9, main.calculateSumOfDigits(12345)); // 1+2+3+4+5 = 15 -> 1+5 = 6
+        assertEquals(3, main.calculateSumOfDigits(987654321)); // 9+8+7+6+5+4+3+2+1 = 45 -> 4+5 = 9
     }
     @Test
     public void testSingleDigitInput() {
-        // Test with a single-digit input
-        assertEquals(5, SumOfDigits.calculateSumOfDigits(5));
+        assertEquals(5, main.calculateSumOfDigits(5));
     }
-
+    @Test
+    public void testTwoDigitInput() {
+        assertEquals(3, main.calculateSumOfDigits(12)); // 1+2 = 3
+    }
 }
